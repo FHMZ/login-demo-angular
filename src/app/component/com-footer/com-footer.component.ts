16f072c4675af27a10e4bcd0f506e297b5299d9e
FHMZ <ferzan17@gmail.com>
2019-02-20 20:25:11 -0300

    footer component

---
+++ src/app/component/com-footer/com-footer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-com-footer',
+  templateUrl: './com-footer.component.html',
+  styleUrls: ['./com-footer.component.css']
+})
+export class ComFooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
