eb87528abe0c2996331c289b5de322a4425e61fb
FHMZ <ferzan17@gmail.com>
2019-02-20 22:40:28 -0300

    navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,9 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-// import { RouterModule, Routes } from '@angular/router';
 
 import {AppComponent} from './app.component';
-import {ComMenuComponent} from './component/com-menu/com-menu.component';
 import {AppRoutingModule, routingComponents} from './app-routing.module';
+import {ComMenuComponent} from './component/com-menu/com-menu.component';
 import {ComFooterComponent} from './component/com-footer/com-footer.component';
 import {ComNavbarComponent} from './component/com-navbar/com-navbar.component';
 
